c6b1af076bcd5d258f344891b88686f181abafcd
panpan <caballerofranz211@gmail.com>
2026-06-27 22:28:44 +0800

    fixed the UI and the responsiveness of the web

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ console.log("Website Loaded");
 
 const header = document.querySelector("h1");
 
+
 header.addEventListener("click", () => {
     alert("Welcome to my portfolio");
+});
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
 });
